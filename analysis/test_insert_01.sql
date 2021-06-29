b3d6b7bf0cb61fcd85033ec817241b8614bb2f84
jon-daniels <jdaniels03079@gmail.com>
2021-06-29 10:17:25 -0400

    Adjusted for cimpress db enviro

--- analysis/test_insert_01.sql
+++ analysis/test_insert_01.sql
@@ -1,1 +1,1 @@
-INSERT INTO mart_demo.dbt_jd_core.rf_coastline_distances (country, distance_km) VALUES (NULL, 5);
+INSERT INTO archetype_sessions.dbt_jd_core.rf_coastline_distances (country, distance_km) VALUES (NULL, 5);
